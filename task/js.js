04c13f7cade9c42100fa603f5ec41378206c88a3
Margarita555 <a_rita@ukr.net>
2022-07-01 19:24:50 +0300

    +

--- task/js.js
+++ task/js.js
@@ -2,24 +2,26 @@ const { src, dest } = require('gulp');
 const babel = require('gulp-babel');
 const webpack = require('webpack-stream');
 const concat = require('gulp-concat');
+const uglify = require('gulp-uglifyjs');
 
 const path = require('../config/path');
 const app = require('../config/app');
 
-// const js = () => {
-//   return (
-//     src(['node_modules/jquery/dist/jquery.js', 'src/libs/slick.min.js', path.js.src], { sourcemaps: app.isDev })
-//       .pipe(concat('main.js'))
-//       .pipe(babel())
-//       // .pipe(webpack(app.webpack))
-//       .pipe(dest(path.js.dest, { sourcemaps: app.isDev }))
-//   );
-// };
 const js = () => {
-  return src(path.js.src, { sourcemaps: app.isDev })
-    .pipe(babel())
-    .pipe(webpack(app.webpack))
-    .pipe(dest(path.js.dest, { sourcemaps: app.isDev }));
+  return (
+    src(['src/libs/jquery/dist/jquery.min.js', 'src/libs/slick.min.js', path.js.src], { sourcemaps: app.isDev })
+      .pipe(concat('libs.min.js'))
+      .pipe(uglify())
+      // .pipe(babel())
+      // .pipe(webpack(app.webpack))
+      .pipe(dest(path.js.dest, { sourcemaps: app.isDev }))
+  );
 };
+// const js = () => {
+//   return src(path.js.src, { sourcemaps: app.isDev })
+//     .pipe(babel())
+//     .pipe(webpack(app.webpack))
+//     .pipe(dest(path.js.dest, { sourcemaps: app.isDev }));
+// };
 
 module.exports = js;
