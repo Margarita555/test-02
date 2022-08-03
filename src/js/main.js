91d7b67670f0ac32cfe6feebe420f21ef3943f28
Margarita555 <a_rita@ukr.net>
2022-08-03 22:58:41 +0300

    +

--- src/js/main.js
+++ src/js/main.js
@@ -78,17 +78,20 @@ function magnify() {
   const zoom = 3;
   let glass, w, h, bw;
 
+  // glass = `<div class="img-magnifier-glass"><img src="./img/projects-images/magnifier.svg" alt="magnifying glass icon" /></div>`;
+  // currentImage.insertAdjacentHTML('beforebegin', glass);
+
   glass = document.createElement('DIV');
   glass.setAttribute('class', 'img-magnifier-glass');
-  console.log('current', currentImage);
-  console.log('current.parent', currentImage.parentElement);
-  console.log('glass', glass);
-  console.log('glass', glass);
+  console.log('a');
   currentImage.parentElement.insertBefore(glass, currentImage);
   glass.style.backgroundImage = "url('" + magnifierIcon.childNodes[0].src + "')";
   glass.style.backgroundRepeat = 'no-repeat';
-  glass.style.left = '200px';
-  glass.style.top = '500px';
+  // glass.style.left = '200px';
+  // glass.style.top = '500px';
+  glass.style.left = '-18px';
+  glass.style.top = '100px';
+  console.log('glass', glass);
   bw = 3;
   // if (event.targetTouches.length == 1) {
   //   var touch = event.targetTouches[0];
@@ -169,8 +172,112 @@ function magnify() {
   }
   function setMagnifyingIcon() {
     glass.style.backgroundImage = "url('" + magnifierIcon.childNodes[0].src + "')";
-    glass.style.backgroundSize = '120px 120px';
+    glass.style.backgroundSize = '80px 80px';
     glass.style.backgroundPosition = '0px 0px';
     glass.style.border = 'none';
   }
 }
+
+// ===============================
+// function magnify() {
+
+//   const currentImage = document.querySelector('.slider-for .slick-current > img');
+//   if (currentImage.parentNode.firstElementChild.classList.contains('img-magnifier-glass')) {
+//     return;
+//   }
+//   magnifierIcon.classList.add('invisible');
+
+//   const zoom = 3;
+//   let glass, w, h, bw;
+
+//   glass = document.createElement('DIV');
+//   glass.setAttribute('class', 'img-magnifier-glass');
+//   console.log('current', currentImage);
+//   console.log('current.parent', currentImage.parentElement);
+//   console.log('glass', glass);
+//   console.log('glass', glass);
+//   currentImage.parentElement.insertBefore(glass, currentImage);
+//   glass.style.backgroundImage = "url('" + magnifierIcon.childNodes[0].src + "')";
+//   glass.style.backgroundRepeat = 'no-repeat';
+//   glass.style.left = '200px';
+//   glass.style.top = '500px';
+//   bw = 3;
+
+//   console.log(glass.offsetWidth, 'offset');
+//   w = glass.offsetWidth / 2;
+//   h = glass.offsetHeight / 2;
+//   console.log('a');
+//   glass.addEventListener('mousemove', moveMagnifier);
+//   currentImage.addEventListener('mousemove', moveMagnifier);
+//   glass.addEventListener('touchstart', moveMagnifier);
+//   glass.addEventListener('touchmove', moveMagnifier);
+//   currentImage.addEventListener('touchmove', moveMagnifier);
+//   console.log('b');
+//   function moveMagnifier(e) {
+//     console.log('c');
+//     var pos, x, y;
+//     e.preventDefault();
+//     pos = getCursorPos(e);
+//     x = pos.x;
+//     y = pos.y;
+//     if (x > currentImage.width - w / zoom) {
+//       x = currentImage.width - w / zoom;
+//       setMagnifyingIcon();
+//     }
+//     if (x < w / zoom) {
+//       x = w / zoom;
+//       setMagnifyingIcon();
+//     }
+//     if (y > currentImage.height - h / zoom) {
+//       y = currentImage.height - h / zoom;
+//       setMagnifyingIcon();
+//     }
+//     if (y < h / zoom) {
+//       y = h / zoom;
+//       setMagnifyingIcon();
+//     } else if (x > 30 && x < 1040 && y > 30 && y < 766) {
+//       glass.style.backgroundImage = "url('" + currentImage.src + "')";
+//       glass.style.borderColor = '#fff';
+//       glass.style.borderWidth = '3px';
+//       glass.style.borderStyle = 'solid';
+//       glass.style.backgroundSize = currentImage.width * zoom + 'px ' + currentImage.height * zoom + 'px';
+//       glass.style.left = x - w + 'px';
+//       glass.style.top = y - h + 'px';
+//       glass.style.backgroundPosition = '-' + (x * zoom - w + bw) + 'px -' + (y * zoom - h + bw) + 'px';
+//     }
+//     console.log('d');
+//   }
+
+//   function getCursorPos(e) {
+//     let a,
+//       x = 0,
+//       y = 0;
+//     e = e || window.event;
+//     a = currentImage.getBoundingClientRect();
+
+//     let pos_x = e.pageX || e.changedTouches[0].pageX;
+//     let pos_y = e.pageY || e.changedTouches[0].pageY;
+//     console.log(pos_x, a.left);
+//     x = pos_x - a.left;
+//     y = pos_y - a.top;
+//     x = x - window.pageXOffset;
+//     y = y - window.pageYOffset;
+//     return { x: x, y: y };
+//     // let a,
+//     //   x = 0,
+//     //   y = 0;
+//     // e = e || window.event;
+//     // a = currentImage.getBoundingClientRect();
+//     // x = e.pageX - a.left;
+//     // y = e.pageY - a.top;
+//     // x = x - window.pageXOffset;
+//     // y = y - window.pageYOffset;
+//     // return { x: x, y: y };
+//   }
+//   function setMagnifyingIcon() {
+//     glass.style.backgroundImage = "url('" + magnifierIcon.childNodes[0].src + "')";
+//     glass.style.backgroundSize = '120px 120px';
+//     glass.style.backgroundPosition = '0px 0px';
+//     glass.style.border = 'none';
+//   }
+// }
